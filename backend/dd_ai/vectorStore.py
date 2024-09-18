a13bd8a908c37f999d1f94a8b3d7bd34d815071f
I-Johnson <subede.johnson@gmail.com>
2024-09-17 21:39:33 -0400

    chunker update

--- backend/dd_ai/vectorStore.py
+++ backend/dd_ai/vectorStore.py
@@ -1,6 +1,13 @@
-from pymilvus import connections
+from pymilvus import connections, db
 
 # Connect to Milvus
 connections.connect(alias="default", host="localhost", port="19530")
+
+vectordb = db.create_database("store_embeddings")
+db.using_database("store_embeddings")
+
+print(db.list_database())
+
+
 
 print("Milvus connected successfully")
